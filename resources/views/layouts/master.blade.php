59f35d705feddb0a07b6d4d3d8b6c27c731a17c0
Jelena Stojsin <ukradenitrenutak@gmail.com>
2018-08-02 23:08:23 -0700

    Zadatak 4

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,13 +9,14 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
                                     integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" 
                                     crossorigin="anonymous">
+        @include('partials.navbar')
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
         
     </head>
     <body>
+        <br><br>
         <div class="flex-center position-ref full-height">
             @yield('content')
         </div>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,5 @@
+<nav class="nav justify-content-center">
+  <a class="nav-link active" href="/movies">Movies</a>
+  <a class="nav-link" href="/add">Add movie</a>
+  
+</nav>
